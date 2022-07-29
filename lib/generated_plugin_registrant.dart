119f69fbcb38fd225a83c59d410f0fadeb882f70
Derda28 <derdasavas67@gmail.com>
2022-07-29 14:51:22 +0300

    SettingsScreen is updated. SettingButton Class is created. BY Sodento

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -8,6 +8,7 @@
 
 import 'package:location_web/location_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -15,5 +16,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 void registerPlugins(Registrar registrar) {
   LocationWebPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
---
+++ lib/widgets/setting_buttons.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../utilities/consts.dart';
+
+class SettingButtons extends StatelessWidget {
+  double textFont = getSize(18);
+  double iconFont = getSize(22);
+  double elevationSize = getSize(4);
+  double borderSize = getSize(10);
+
+  void Function() itemClicked;
+  IconData? selectedIcon;
+  String? assetImage;
+  String buttonTitle;
+  Color? textColor;
+  Color? iconColor;
+
+  SettingButtons(
+      {Key? key,
+        required this.itemClicked,
+        this.selectedIcon,
+        this.assetImage,
+        this.iconColor,
+        this.textColor,
+        required this.buttonTitle})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      style: OutlinedButton.styleFrom(
+          primary: Colors.black38,
+          side: BorderSide(color: Colors.black38, width: getSize(2)),
+          shadowColor: Colors.white,
+          elevation: elevationSize,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(borderSize))),
+          backgroundColor: Colors.white),
+      onPressed: itemClicked,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+
+          assetImage != null? Image(
+            width: iconFont,
+            height: iconFont,
+            color: iconColor != null? Colors.red : fontColor,
+            image: AssetImage(assetImage!),
+          ):Icon(selectedIcon, color: iconColor != null? Colors.red : fontColor, size: iconFont),
+          Text(
+            buttonTitle,
+            style: TextStyle(color: textColor != null? Colors.red : fontColor, fontSize: textFont),
+          )
+        ],
+      ),
+    );
+  }
+}
